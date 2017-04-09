03877a220faabefcbdf7c33db73e20d678b2beea
Harsha Ivaturi <harsha.ivaturi@gmail.com>
2017-04-10 00:41:27 +0800

    Add support for offline conversion

--- src/converters/currencies.js
+++ src/converters/currencies.js
@@ -1,34 +1,50 @@
 import fx from 'money';
-import { curry, map, zipWith, last, fromPairs } from 'ramda';
+import { curry, map, zipWith, last, fromPairs, take, compose, reject, equals } from 'ramda';
 // import conversions from '../helpers/conversions';
 import supportedCurrencies from '../helpers/constants';
 import formatOutput from '../helpers/format-output';
 
 function fetchConversionRates(curencyList) {
   return fetch(`https://query.yahooapis.com/v1/public/yql?q=select+*+from+yahoo.finance.xchange+where+pair+=+%22${curencyList.join(',')}%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=`)
-    .then(res => res.json());
+    .then(res => {
+      if (res.ok) {
+        return res.json();
+      } else {
+        console.error('Unable to fetch conversion rate, falling back to cached conversion rates. ELSE');
+        new Promise.reject(null);
+      }
+    })
+    .catch(err => {
+      console.error('Unable to fetch conversion rate, falling back to cached conversion rates. Then/Catch');
+      return Promise.resolve(JSON.parse(localStorage.getItem('rates')));
+    });
 }
 
 async function setupMoney(currencyList, base = 'USD') {
   const response = await fetchConversionRates(currencyList);
+  localStorage.setItem('rates', JSON.stringify(response));
   const extractRate = ({ Name, Rate }) => ([[last(Name.split('/'))], +Rate]);
   fx.base = base;
   fx.rates = fromPairs(map(extractRate, response.query.results.rate));
 }
 
-export default (amount, fromCurr, toCurr) => {
+export default (amount, from, to) => {
   setupMoney(supportedCurrencies);
 
-  const convertTo = curry(to => fx.convert(amount, { from: fromCurr, to }));
+  const convertTo = curry(toCurr => fx.convert(amount, { from, to: toCurr }));
   const formatCurrency = formatOutput('currency');
+  const priorityCurrencies = compose(
+    take(8),
+    reject(equals(to)),
+  )(supportedCurrencies);
 
   return {
-    orig: formatCurrency(amount, fromCurr),
-    conversion: formatCurrency(convertTo(toCurr || 'USD'), toCurr),
+    orig: formatCurrency(amount, from),
+    conversion: formatCurrency(convertTo(to || 'USD'), to),
     possibleConversions: zipWith(
       formatCurrency,
-      map(convertTo, supportedCurrencies),
-      supportedCurrencies,
+      map(convertTo, priorityCurrencies),
+      priorityCurrencies,
     ),
     category: 'money',
   };
--- src/helpers/format-output.js
+++ src/helpers/format-output.js
@@ -18,7 +18,3 @@ export default (conversionType) => {
 
   return (amt, unit) => ({ amt, unit });
 };
-
-
-
-
